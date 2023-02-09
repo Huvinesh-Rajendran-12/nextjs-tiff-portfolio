ceb76526586b3ee3d5c5286cb4d2e6893e6c7afd
Huvinesh Rajendran <huvinesh@teleme.co>
2023-02-09 17:54:58 +0800

    API integration

--- tiffany-portfolio/pages/work/page3.tsx
+++ tiffany-portfolio/pages/work/page3.tsx
@@ -2,11 +2,13 @@
 import Link from 'next/link';
 import PrimaryButton from '../../components/Button';
 import img from 'next/image';
-import { useFontStore } from '../../store/store';
+import { useFontStore, usePagesStore } from '../../store/store';
 import { useEffect } from 'react';
 
 export default function PageThree() {
   const fontStore = useFontStore((state) => state.font)
+  const pageThreeMetadata = usePagesStore((state) => state.pagesData ? state.pagesData[2].metadata : undefined)
+  const pageFourPic = usePagesStore((state) => state.pagesData ? state.pagesData[1].metadata.pic_1 : undefined)
   console.log(fontStore?.metadata.title.value)
   useEffect(() => {
     document.querySelector<HTMLElement>('.font-animation')?.style.setProperty('--font-animation', fontStore?.metadata.animation.value ?? '')
@@ -17,23 +19,18 @@ export default function PageThree() {
     <div className='flex flex-col'>
       <section className='py-10 lg:pt-[9.88vh] lg:pb-[10vh] px-5 lg:px-[8.89vw]'>
         <div className='flex flex-col justify-center text-center'>
-          <h1 className='lg:text-5xl sm:text-3xl uppercase font-title'>Pink4Hope</h1>
+          <h1 className='lg:text-5xl sm:text-3xl uppercase font-title'>{pageThreeMetadata?.title}</h1>
           <div className='text-white flex flex-row h-8 justify-center gap-x-2'>
-            <div className='uppercase font-content'>Brand Identity</div>
+            <div className='uppercase font-content'>{pageThreeMetadata?.tag_1}</div>
             <div className='vertical-line' />
-            <div className='uppercase font-content'>Social Media</div>
+            <div className='uppercase font-content'>{pageThreeMetadata?.tag_2}</div>
           </div>
         </div>
       </section>
       <section className='text-2xl text-center justify-center flex flex-row text-white pt-2'>
         <div className='flex flex-col gap-y-8'>
           <p className='max-w-lg font-content'>
-              Your brand needs to reflect who you are and what makes you
-              different. That’s why I use design to take your business to new
-              levels by creating a visual identity that complements your brand
-              values, goals and personality. At visual identity design, I will
-              take care of all the hard stuff so you can focus on what&aposs
-              important.{' '}
+              {pageThreeMetadata?.description}{' '}
           </p>
           <div className='text-white text-center justify-center'>
             <div className='font-content'>Client: Diamond Platinum</div>
@@ -47,21 +44,21 @@ export default function PageThree() {
           <img
             alt=""
             src={
-              'https://imgix.cosmicjs.com/abb42a30-8c10-11ed-bac9-7fe1734a16aa-TC1.jpeg'
+              pageThreeMetadata?.pic_1.imgix_url
             }
             className='w-full lg:w-1/2 lg:min-w-[400px] block ml-auto mr-auto'
           />
           <img
             alt=""
             src={
-              'https://imgix.cosmicjs.com/8da57640-8beb-11ed-bac9-7fe1734a16aa-PinkOct2.jpeg'
+              pageThreeMetadata?.pic_2.imgix_url
             }
             className='w-full lg:w-1/2 block ml-auto mr-auto'
           />
           <img
             alt=""
             src={
-              'https://imgix.cosmicjs.com/92e94550-8beb-11ed-bac9-7fe1734a16aa-PinkOct3.jpg'
+              pageThreeMetadata?.pic_3.imgix_url
             }
             className='w-full lg:w-1/2 block ml-auto mr-auto'
           />
@@ -70,16 +67,16 @@ export default function PageThree() {
       <section className='relative h-[90vh]'>
             <div className='overflow-hidden h-full lg:pt-[12.35vh] flex flex-col space-y-6 justify-between lg:block'>
             <p className='whitespace-nowrap uppercase font-medium text-[30vw] md:text-[25vw] pb-8 lg:text-xxl1 leading-1 lg:leading-[15.28vw]'> 
-              <span className='text-scrolling font-animation'>The Power of Visual Communication</span>
-              <span className='text-scrolling font-animation'>The Power of Visual Communication</span>
+              <span className='text-scrolling font-animation'>{pageThreeMetadata?.animation_1}</span>
+              <span className='text-scrolling font-animation'>{pageThreeMetadata?.animation_1}</span>
             </p>
             <p className='whitespace-nowrap uppercase font-medium text-[40vw] md:text-[25vw] lg:text-xxl1 leading-1 lg:leading-[15.28vw]'>
-              <span className='reverse text-scrolling font-animation'>The Power of Visual Communication</span>
+              <span className='reverse text-scrolling font-animation'>{pageThreeMetadata?.animation_2}</span>
               <span></span>
             </p>
             </div>
             <div className='absolute top-10 left-0 flex flex-row justify-center w-full h-full'>
-              <img alt='' src={'https://imgix.cosmicjs.com/ca11b9e0-8c13-11ed-bac9-7fe1734a16aa-Vitavally1.jpeg'} className='md:w-1/3 h-[80vh] sm:w-1/2  max-h-200px' />
+              <img alt='' src={pageFourPic?.imgix_url} className='md:w-1/3 h-[80vh] sm:w-1/2  max-h-200px' />
             </div>
       </section>
     </div>
