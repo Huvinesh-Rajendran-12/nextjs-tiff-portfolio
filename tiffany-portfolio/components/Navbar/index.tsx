ccac664233b77262cf32e1223f2e89186cd4947e
Huvinesh Rajendran <huvinesh@teleme.co>
2023-03-04 01:58:39 +0800

    feat: Changing layout v3

--- tiffany-portfolio/components/Navbar/index.tsx
+++ tiffany-portfolio/components/Navbar/index.tsx
@@ -2,6 +2,7 @@ import { useDisclosure } from '@chakra-ui/react'
 import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 import Logo from '../Logo'
 import Link from 'next/link';
+import { useState } from 'react';
 
 interface NavItemsProps {
   isOpen: boolean
@@ -11,7 +12,7 @@ const NavItems = ({isOpen}:NavItemsProps) => {
   const smallDisplay = isOpen ? 'sm:flex sm:flex-col' : 'sm:hidden';
   return (
     <div
-      className={`md:flex md:flex-row h-8 items-center ${smallDisplay} w-fit md:gap-x-5`}
+      className={`md:h-8 md:flex md:flex-row items-center ${smallDisplay} w-fit md:gap-x-5`}
     >
       <div className='vertical-line'></div>
       <Link href='/about'>About</Link>
@@ -22,18 +23,21 @@ const NavItems = ({isOpen}:NavItemsProps) => {
 }
 
 export default function Navbar() {
-  const {isOpen, onOpen, onClose} = useDisclosure();
+  // const {isOpen, onOpen, onClose} = useDisclosure();
+  const [isNavOpen, setIsNavOpen] = useState<boolean>(false)
   return (
-    <header className='navbar-border sticky z-20 top-0 flex bg-black flex-row justify-between px-4 py-2 items-center'>
+    <div className='flex flex-col justify-center navbar-border'>
+    <header className='sticky z-20 top-0 flex bg-black flex-row justify-between px-4 py-2 items-center'>
       <div>
           <Link href='/'>
           <Logo/>
           </Link>
       </div>
-      <button className='sm:block md:hidden'>
-          {isOpen ? <CloseIcon/> : <HamburgerIcon/>}
+      <button className='sm:block md:hidden' onClick={() => {isNavOpen ? setIsNavOpen(false) : setIsNavOpen(true)}}> 
+          {isNavOpen ? <CloseIcon/> : <HamburgerIcon/>}
       </button>
-      <NavItems isOpen={isOpen}/>
+      <NavItems isOpen={isNavOpen}/>
     </header>
+    </div>
   )
 }
--- tiffany-portfolio/pages/services.tsx
+++ tiffany-portfolio/pages/services.tsx
@@ -55,8 +55,8 @@ export default function ServicesPage({ServicesPageData}: ServicesPageProps) {
               className='rounded-md'
             />
             <div>
-              <h1 className='text-white'>1</h1>
-              <h1 className='text-white font-medium py-2'>
+              <h1 className='text-white text-center'>1</h1>
+              <h1 className='text-white text-center font-medium py-2'>
                 BRAND IDENTITY
               </h1>
               <div className='max-w-sm'>
@@ -67,7 +67,7 @@ export default function ServicesPage({ServicesPageData}: ServicesPageProps) {
                   values, goals and personality.
                 </p>
               </div>
-              <h1>BRAND ASSETS</h1>
+              <h1 className='text-center'>BRAND ASSETS</h1>
               <ul className='list-disc ml-6'>
                 <li>Brand Moodboard</li>
                 <li>
@@ -93,11 +93,11 @@ export default function ServicesPage({ServicesPageData}: ServicesPageProps) {
       <section>
         <div className='pt-20'>
           <div
-            className='px-4 space-x-10 flex flex-col-reverse lg:flex-row items-center pb-2 justify-center'
+            className='flex-col-reverse lg:flex-row flex items-center justify-center px-3 gap-y-4'
           >
-            <div className='text-left'>
-              <h1 color={'secondary'}>2</h1>
-              <h1 className='text-white py-2'>
+            <div className='text-left gap-y-2'>
+              <h1 color={'secondary'} className='text-center'>2</h1>
+              <h1 className='text-white py-2 text-center'>
                 BRAND IDENTITY
               </h1>
               <div className='max-w-sm'>
@@ -108,7 +108,7 @@ export default function ServicesPage({ServicesPageData}: ServicesPageProps) {
                   values, goals and personality.
                 </div>
               </div>
-              <h1>BRAND ASSETS</h1>
+              <h1 className='text-center'>BRAND ASSETS</h1>
               <ol className='list-disc ml-6'>
                 <li color={'secondary'}>Brand Moodboard</li>
                 <li color={'secondary'}>
@@ -127,7 +127,6 @@ export default function ServicesPage({ServicesPageData}: ServicesPageProps) {
                   <div>Printed + Digital Collateral</div>
                 </li>
               </ol>
-              
             </div>
             <img
               alt=""
@@ -139,13 +138,13 @@ export default function ServicesPage({ServicesPageData}: ServicesPageProps) {
       </section>
       <section>
         <div className='overflow-hidden h-full lg:pt-[12.35vh] flex flex-col space-y-6 justify-between lg:block'>
-          <p className='whitespace-nowrap uppercase font-medium text-[30vw] md:text-[25vw] pb-8 lg:text-xxl1 leading-1 lg:leading-[15.28vw]'> 
+          <p className='whitespace-nowrap uppercase font-medium text-[30vw] md:text-[25vw] pb-4 lg:text-xxl1 leading-1 lg:leading-[15.28vw]'> 
             <span className='text-scrolling font-animation'>The Power of Visual Communication</span>
             <span className='text-scrolling font-animation'>The Power of Visual Communication</span>
           </p>
         </div>
       </section>
-      <section className='py-10'>
+      <section className='py-4'>
         <div
           className='text-2xl text-center justify-center flex flex-row text-white pt-2'
         >
@@ -155,12 +154,15 @@ export default function ServicesPage({ServicesPageData}: ServicesPageProps) {
           </p>
         </div>
       </section>
-    <div className='py-2'>
+      <section>
+        
+      </section>
+    <div className='py-10'>
           
       </div>
       <div className='py-2 px-2 flex-col' >
         <div
-          className='flex lg:flex-row flex-col h-14 items-center justify-center text-center gap-x-2 '
+          className='flex lg:flex-row flex-col h-14 items-center justify-center text-center gap-x-2 sm:space-y-2 '
         >
           <div color={'secondary'} className='text-white'>
             <div>Step 1</div>
@@ -188,7 +190,7 @@ export default function ServicesPage({ServicesPageData}: ServicesPageProps) {
           <a href="mailto:imtiffanycfy@gmail.com">
             <ContactButton
               text="Contact Me"
-              className="border-white border rounded-md py-2 px-2"
+              className="border-white border rounded-md mt-16 py-2 px-2"
             />
           </a>
         </div>
