ce91629dd0edbf03654e9a0ef961425086252142
Huvinesh Rajendran <huvinesh@teleme.co>
2023-03-04 09:41:56 +0800

    Navbar layout changed

--- tiffany-portfolio/components/Navbar/index.tsx
+++ tiffany-portfolio/components/Navbar/index.tsx
@@ -9,10 +9,10 @@ interface NavItemsProps {
 }
 
 const NavItems = ({isOpen}:NavItemsProps) => {
-  const smallDisplay = isOpen ? 'sm:flex sm:flex-col' : 'sm:hidden';
+  const smallDisplay = isOpen ? 'sm:flex sm:flex-col w-full' : 'sm:hidden';
   return (
     <div
-      className={`md:h-8 md:flex md:flex-row items-center ${smallDisplay} w-fit md:gap-x-5`}
+      className={`md:h-8 block md:flex md:flex-row items-center ${smallDisplay} md:w-fit md:gap-x-5`}
     >
       <div className='vertical-line'></div>
       <Link href='/about'>About</Link>
@@ -26,18 +26,16 @@ export default function Navbar() {
   // const {isOpen, onOpen, onClose} = useDisclosure();
   const [isNavOpen, setIsNavOpen] = useState<boolean>(false)
   return (
-    <div className='flex flex-col justify-center navbar-border'>
-    <header className='sticky z-20 top-0 flex bg-black flex-row justify-between px-4 py-2 items-center'>
-      <div>
-          <Link href='/'>
-          <Logo/>
+    <nav className='navbar-border w-full sticky z-20 top-0 flex flex-wrap bg-black justify-between px-4 py-2 items-center'>
+      <div className='w-[400px]'>
+          <Link href='/' className='w-fit'>
+            <Logo/>
           </Link>
       </div>
       <button className='sm:block md:hidden' onClick={() => {isNavOpen ? setIsNavOpen(false) : setIsNavOpen(true)}}> 
           {isNavOpen ? <CloseIcon/> : <HamburgerIcon/>}
       </button>
       <NavItems isOpen={isNavOpen}/>
-    </header>
-    </div>
+    </nav>
   )
 }
