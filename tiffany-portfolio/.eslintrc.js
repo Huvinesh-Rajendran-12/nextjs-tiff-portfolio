6c914f35a40479b98eb8bad3a71f5d658bb51e91
Huvinesh Rajendran <huvinesh@teleme.co>
2023-04-11 11:19:33 +0800

    chore: Fixed eslint error

--- tiffany-portfolio/.eslintrc.js
+++ tiffany-portfolio/.eslintrc.js
@@ -10,5 +10,11 @@ module.exports = {
     project: true,
     tsconfigRootDir: __dirname,
   },
+  rules: {
+    '@typescript-eslint/no-unsafe-return': off,
+    '@typescript-eslint/no-unsafe-call':off,
+    '@typescript-eslint/no-unsafe-member-access':off,
+    '@typescript-eslint/no-unsafe-assignment':off
+  },
   root: true,
 };
