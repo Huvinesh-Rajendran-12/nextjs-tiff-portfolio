77613776f953d758f692e48f4df9d93c6406b563
Huvinesh Rajendran <huvinesh@teleme.co>
2023-04-11 10:54:52 +0800

    chore: Fixed error

--- tiffany-portfolio/pages/_app.tsx
+++ tiffany-portfolio/pages/_app.tsx
@@ -1,14 +1,9 @@
-import { ChakraProvider } from '@chakra-ui/react';
 import '../styles/globals.css';
 import '../styles/animation.css';
 import '../styles/main.css';
 import type { AppProps } from 'next/app';
-import extendedTheme from '../theme';
-import { Router } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
-import { getFontConfig } from '../lib/fontAPI';
-import { useFontStore } from '../store/store';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
