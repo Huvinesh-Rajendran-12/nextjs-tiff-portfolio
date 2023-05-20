a373171ef1e0d2e9933d3d5beac0248d2d3bfe37
Huvinesh Rajendran <huvinesh@teleme.co>
2023-05-21 01:41:24 +0800

    chore: Fixing icon error

--- tiffany-portfolio/pages/_app.tsx
+++ tiffany-portfolio/pages/_app.tsx
@@ -15,7 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
           <link rel="manifest" href="/site.webmanifest"/>
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ffff"/>
           <meta name="description" content="Tiffany C. is a Malaysian creative designer and marketing professional, specialising in graphic design, branding, and social media marketing. With expertise in developing and refining brand identities, she creates promotional materials that communicate her clients' messages effectively, enhancing their online presence and driving revenue growth."></meta>
           <meta property="og:title" content="TIFFANYCFY" />
           <meta property="og:description" content="Tiffany C. is a Malaysian creative designer and marketing professional, specialising in graphic design, branding, and social media marketing. With expertise in developing and refining brand identities, she creates promotional materials that communicate her clients' messages effectively, enhancing their online presence and driving revenue growth." />
