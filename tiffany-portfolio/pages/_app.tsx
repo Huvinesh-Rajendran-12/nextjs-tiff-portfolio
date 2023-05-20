166448c1e8ff7ff19ff5678dfdbb1bcea0aee8d0
Huvinesh Rajendran <huvinesh@teleme.co>
2023-05-21 02:17:21 +0800

    Revert "chore: Try meta tags"
    This reverts commit 4bef292da7e5f2e1e63f609dd84f9fa243af282d.

--- tiffany-portfolio/pages/_app.tsx
+++ tiffany-portfolio/pages/_app.tsx
@@ -5,12 +5,11 @@ import type { AppProps } from 'next/app';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Head from 'next/head';
-import { Suspense } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Suspense>
-      <Head>
+      <div className='bg-black text-white overflow-x-hidden overflow-y-scroll h-screen'>
+        <Head>
           <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png"/>
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
@@ -19,8 +18,7 @@ export default function App({ Component, pageProps }: AppProps) {
           <meta name="description" content="Tiffany C. is a Malaysian creative designer and marketing professional, specialising in graphic design, branding, and social media marketing. With expertise in developing and refining brand identities, she creates promotional materials that communicate her clients' messages effectively, enhancing their online presence and driving revenue growth."></meta>
           <meta property="og:title" content="TIFFANYCFY" />
           <meta property="og:description" content="Tiffany C. is a Malaysian creative designer and marketing professional, specialising in graphic design, branding, and social media marketing. With expertise in developing and refining brand identities, she creates promotional materials that communicate her clients' messages effectively, enhancing their online presence and driving revenue growth." />
-      </Head>
-      <div className='bg-black text-white overflow-x-hidden overflow-y-scroll h-screen'>
+        </Head>
         <Navbar />
         <div className='scroll-container scroll-smooth ready'>
           <main>
@@ -29,6 +27,5 @@ export default function App({ Component, pageProps }: AppProps) {
           <Footer/>
         </div>
       </div>
-    </Suspense>
   );
 }
